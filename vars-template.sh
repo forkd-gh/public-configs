b1bd5a0385e976d2675ea74e34eba953bbccc5c4
Forked-Logic <justmergeit.eoc@gmail.com>
2025-05-18 20:33:25 -0600

    Fix Bad Vars Template

--- vars-template.sh
+++ vars-template.sh
@@ -3,10 +3,10 @@
 #--------------------------------------------------------------------
 
 # Likely will want to change:
-REPO="/home/"$(logname)"/Github/public-configs" # Where this repo is: ex. "/home/$USER/Github/public-configs"
+REPO="/home/$(logname)/Github/public-configs" # Where this repo is: ex. "/home/$USER/Github/public-configs"
 
 # Likely won't want to change:
 RUNFOR="$(logname)" # User to run this for: ex. "$(logname)"
-CONF="/home/"$(logname)"/.config" # Location of the RUNFOR users config dotfiles
+CONF="/home/$(logname)/.config" # Location of the RUNFOR users config dotfiles
 
 #--------------------------------------------------------------------
