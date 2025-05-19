cda2e1e48fca5d85bfff842c5963cdccdbb8d321
Forked-Logic <justmergeit.eoc@gmail.com>
2025-05-18 22:54:40 -0600

    A more tolerable menu

--- wofi/menu-toggle.sh
+++ wofi/menu-toggle.sh
@@ -1,12 +1,9 @@
-#!/usr/bin/env nix-shell
-#!nix-shell -i bash -p wofi procps
+#!/bin/bash
 
-# ~/.config/hypr/menu-toggle.sh
-
-wofi_pid=$(pgrep -f "wofi --show drun")
+wofi_pid=$(pgrep -f "wofi.*--show drun")
 
 if [[ -n "$wofi_pid" ]]; then
-    pkill -f "wofi --show drun"
+    pkill -f "wofi.*--show drun"
 else
-    wofi --show drun &
+    wofi -a --show drun &
 fi
